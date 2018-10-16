4cda09a3353133670d47401e0f55eed9e9bc4a18
Hamish Lees <hamishlees6@gmail.com>
2018-10-16 15:46:19 +1100

    decode and encode done, might turn to class soon

--- hash_converter.rb
+++ hash_converter.rb
@@ -1,6 +1,6 @@
-def hex_to_dec(array)
-    tens = array.collect {|hex| hex[0]}
-    ones = array.collect {|hex| hex[1]}
+def decode_message(hexadecimal)
+    tens = hexadecimal.collect {|hex| hex[0]}
+    ones = hexadecimal.collect {|hex| hex[1]}
     replacements = [ ["A", "10"], ["B", "11"], ["C", "12"], ["D", "13"], ["E", "14"], ["F", "15"] ]
     tens.collect {|hex| replacements.each {|replacement| hex.gsub!(replacement[0], replacement[1])}}
     ones.collect {|hex| replacements.each {|replacement| hex.gsub!(replacement[0], replacement[1])}}
@@ -9,10 +9,24 @@ def hex_to_dec(array)
     numbers = ones.zip(tens).map{|ten, one| ten + one}
     number_codes = (1..26).to_a.push(32).push((97..122).to_a).flatten
     letter_codes = *('A'..'Z').to_a.push(' ').push(('a'..'z').to_a).flatten
-    result = []
-    numbers.collect {|num| result.push(letter_codes[number_codes.index(num)])}
-    puts result.join
+    puts numbers.collect! {|num| (letter_codes[number_codes.index(num)])}.join
 end
 
-array = %w(09 74 20 69 73 20 17 65 64 6E 65 73 64 61 79 20 6D 79 20 64 75 64 65 74)
-hex_to_dec(array)
+hexadecimal = %w(09 74 20 69 73 20 17 65 64 6E 65 73 64 61 79 20 6D 79 20 64 75 64 65 74)
+decode_message(hexadecimal)
+
+
+def encode_message(message)
+    letters = ('A'..'Z').to_a.push(' ').to_a.push(('a'..'z').to_a).flatten
+    numbers = (1..26).to_a.push(32).push((97..122).to_a).flatten
+    result = []
+    replacements = [ ["A", "10"], ["B", "11"], ["C", "12"], ["D", "13"], ["E", "14"], ["F", "15"] ]
+    message.each_char {|letter| result.push(numbers[letters.index(letter)])}
+    result.collect! {|num| (num / 16).to_s + (num % 16).to_s}
+    ones = result.collect {|one| one.slice!(0)}
+    result.collect {|hex| replacements.each {|replacement| hex.gsub!(replacement[1], replacement[0])}}
+    puts ones.zip(result).map{|ten, one| (ten + one)}
+end
+
+message = "It is Wednesday my dudet"
+encode_message(message)
